fc39a0c359009e87354ad67821391e985ea82c0b
lufer0314 <lufer0314@gmail.com>
2020-04-21 20:41:21 -0500

    agregar areas

---
+++ src/gNumericTypes/BotellasAgua.java
@@ -0,0 +1,25 @@
+package gNumericTypes;
+
+import libs.Input;
+
+public class BotellasAgua {
+
+    public static void main(String[] args) {
+
+        int botellasxmin = 12;
+        System.out.print(" ¿Cuánto minutos tardas en bañarte? ");
+        int minutos = Input.get_int();
+
+        if (minutos > =0) {
+            System.out.print("El número de botellas que consumiste al bañarte son: " + botellasxmin * minutos);
+        } else if {
+
+            System.out.print("Ingresar un numero positivo ");
+
+
+        }
+    }
+}
+
+
+
